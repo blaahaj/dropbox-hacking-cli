6e3e58d1e5bb6a5a16ef26cc57782f5346dc0fd8
Rachel Evans <rachel@rachelevans.org>
2025-05-29 12:59:02 +0200

    feat: import from monorepo

---
+++ src/operations/mv.ts
@@ -0,0 +1,39 @@
+import {
+  DropboxProvider,
+  GlobalOptions,
+  writeStdout,
+} from "@blaahaj/dropbox-hacking-util";
+
+import type { Handler } from "@blaahaj/dropbox-hacking-util/cli";
+
+const verb = "mv";
+
+// Case insensitive
+// Can't rename to same-case (e.g. mv foo FOO) - you have to do e.g. foo => tmpfoo => FOO
+// Does a "mkdir -p" on the destination structure
+
+const handler: Handler = async (
+  dbxp: DropboxProvider,
+  argv: string[],
+  _globalOptions: GlobalOptions,
+  usageFail: () => void,
+): Promise<void> => {
+  if (argv.length !== 2) usageFail();
+  const fromPath = argv[0];
+  const toPath = argv[1];
+
+  const dbx = await dbxp();
+
+  dbx
+    .filesMoveV2({ from_path: fromPath, to_path: toPath })
+    .then((response) => writeStdout(JSON.stringify(response.result) + "\n"))
+    .then(() => process.exit(0))
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+};
+
+const argsHelp = "FROM_DROPBOX_PATH TO_DROPBOX_PATH";
+
+export default { verb, handler, argsHelp };
